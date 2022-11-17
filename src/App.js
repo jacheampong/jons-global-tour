6d5fa8907a6e4cd7c5f15fcd069e188d86f10d27
jacheampong <jacheampong@gmail.com>
2022-11-17 10:54:56 -0500

    App title update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Home from './Home';
-import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
-    <h2>Jons Global Tour</h2>
+      <header>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </header>
+      <h2>Jons Global Tour</h2>
       <Home />
     </div>
   );
