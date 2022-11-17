ad1ef31ae03d05d5d05aae018676bddfba970799
jacheampong <jacheampong@gmail.com>
2022-11-17 00:38:10 -0500

    Initial files

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <>
+            <main>
+                <p>
+                    A journey of a thousand miles begins with a single step! 
+
+                    <br/>
+                    Chinese proverb
+                </p>
+            </main>
+        </>
+    )
+}
---
+++ src/common/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <>
+        <h1>Footer here</h1>
+            <Footer>
+                @2022 Are you not entertained!
+            </Footer>
+        </>
+    )
+}
